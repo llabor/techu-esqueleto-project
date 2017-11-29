1b52659fc78d0dafcc8796fc283fd30c69e7c5e2
Dolores Fuertes <dolores@fuertesdebarriga.com>
2017-11-29 19:17:34 +0100

    Server JS arreglado

--- back/server.js
+++ back/server.js
@@ -3,15 +3,16 @@ var express = require('express'),
   port = process.env.PORT || 3000;
 
 //variable para poder usar el request-json
-var requestJson = require('request-json');
+var requestjson = require('request-json');
 //variable para que se pueda parsear el req.body y no salga como undefined
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 
 var path = require('path');
 
-var urlmovimientosMlab = "https://api.mlab.com/api/1/databases/bdbanca/collections/movimientos?apiKey=50c5ea68e4b0a97d668bc84a";
-var clienteMlab = requestjson.createClient(urlmovimientosMlab);
+var urlmovimientosMlab = "https://api.mlab.com/api/1/databases/bdbanca/collections/movimientos";
+var apiKey = "apiKey=50c5ea68e4b0a97d668bc84a";
+var clienteMlab = requestjson.createClient(urlmovimientosMlab + "?" + apiKey);
 
 app.listen(port);
 
@@ -19,6 +20,7 @@ console.log('todo list RESTful API server started on: ' + port);
 
 app.get('/movimientos', function(req, res) {
   //res.sendFile(path.join(__dirname, 'index.html'));
+  clienteMlab = requestjson.createClient(urlmovimientosMlab + "?" + apiKey);
     clienteMlab.get('', function(err, resM, body) {
       if(err)
       {
@@ -33,7 +35,8 @@ app.get('/movimientos', function(req, res) {
 
 app.get('/clientes', function(req, res) {
   //res.sendFile(path.join(__dirname, 'index.html'));
-    clienteMlab.get('&f={"idcliente":1, "nombre": 1, "apellidos": 1}', function(err, resM, body) {
+  clienteMlab = requestjson.createClient(urlmovimientosMlab + "?f={'idcliente':1, 'nombre': 1, 'apellidos': 1}" + "&" + apiKey);
+    clienteMlab.get('', function(err, resM, body) {
       if(err)
       {
         console.log(body);
